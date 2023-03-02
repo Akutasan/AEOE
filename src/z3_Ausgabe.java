a73535b414e16cda3c6420ca4b860fb55247c4e4
Akutasan <mirodurana1@gmail.com>
2023-03-02 09:35:48 +0100

    z3Ausgabe

--- src/z3_Ausgabe.java
+++ src/z3_Ausgabe.java
@@ -1,21 +1,25 @@
-import java.awt.*;
 import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
 
 /**
  * Beschreiben Sie hier die Klasse jksydkfgiiuzacsfhlk.
- * 
- * @author (Ihr Name) 
+ *
+ * @author (Ihr Name)
  * @version (eine Versionsnummer oder ein Datum)
  */
-public class z3_Ausgabe
-{
+public class z3_Ausgabe {
+
+    public static void main(String[] args){
+        Z3("A");
+    }
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    /*private*/ static void Z3(String Planet, String Q) 
-    {
+    /*private*/
+    static void Z3(String Planet) {
         {
             //Variablen
             int menge = 3;
-            
+
             //Farbschema/Panel
             UIManager UI = new UIManager();
             UI.put("OptionPane.messageForeground", Color.white);
@@ -23,25 +27,35 @@ public class z3_Ausgabe
             UI.put("OptionPane.background", new Color(31, 99, 151));
             UI.put("Panel.background", new Color(19, 60, 91));
 
+            databaseConnection(Planet);
+
             //Ausgabetext fürs Panel
             String html = "<html><body width='%1s'><h1>Abschlussbericht des ExoProjekts</h1>"
-                + Planet + "<h2></h2>"
-                //erstellen einer Tabelle für eine übersichtliche Ausgabe
-                + "<table>"
-                + "<tr><td>QuadrantenName</td><td>Gold</td><td>Silber</td><td>Uran</td><td>Kupfer</td><td>Zink</td><td>Total</td><td>Bewertung</td></tr>";
-                //Dateien werden in die Tabelle eingetragen
-            for(int i = 0; i < menge; i++)
-            {
+                    + "Planet: " + Planet + "<h2></h2>"
+                    //erstellen einer Tabelle für eine übersichtliche Ausgabe
+                    + "<table>"
+                    + "<tr><td>QuadrantenName</td><td>Gold</td><td>Silber</td><td>Uran</td><td>Kupfer</td><td>Zink</td><td>Total</td><td>Bewertung</td></tr>";
+            //Dateien werden in die Tabelle eingetragen
+            for (int i = 0; i < menge; i++) {
                 //Spalten für jeden Quadranten ausfüllen
-                html = html + "<tr><td>"+"Name"+"</td><td>"+"Goldwert"+"</td><td>"+"Silberwert"+"</td><td>"+"Uranwert"+"</td><td>"+"Kupferwert"+"</td><td>"+"Zinkwert"+"</td><td>"+"Totalwert"+"</td><td>"+"Bewertungswert"+"</td></tr>";
+                html = html + "<tr><td>" + "Name" + "</td><td>" + "Goldwert" + "</td><td>" + "Silberwert" + "</td><td>" + "Uranwert" + "</td><td>" + "Kupferwert" + "</td><td>" + "Zinkwert" + "</td><td>" + "Totalwert" + "</td><td>" + "Bewertungswert" + "</td></tr>";
             }
-                
-                html = html + "</table>";
-	
+
+            html = html + "</table>";
+
             //Weite
             int w = 500;
-            
+
             JOptionPane.showMessageDialog(null, String.format(html, w, w), "Ziel 3 - ExoDatenbank", JOptionPane.INFORMATION_MESSAGE, null);
         }
+    }
+
+    //Verbindung zur Datenbank wird hergestellt
+    static void databaseConnection(String planet) {
+        if (!MySQL.isConnected()) {
+            MySQL.connect();
+        }
+        MySQL.getObject(planet, "Name", "Planet", "*");
+        MySQL.close();
     }
 }
